036429e2b87935a48726432a1558dad3f7fa066c
marcolaxyz7 <marcoantoniogorzoni34@gmail.com>
2025-11-30 19:40:31 -0300

    parte online

--- server.js
+++ server.js
@@ -16,23 +16,24 @@ const players = {};
 io.on('connection', (socket) => {
   console.log('Piloto conectado:', socket.id);
 
-  // Dados do jogador
+  // Dados do jogador (Agora inclui 'thrust')
   players[socket.id] = {
     x: 0, y: 5, z: 0,
     qx: 0, qy: 0, qz: 0, qw: 1,
-    color: Math.random() * 0xffffff
+    color: Math.random() * 0xffffff,
+    thrust: false // Estado do motor
   };
 
-  // Envia estado atual
+  // Envia estado atual para quem entrou
   socket.emit('currentPlayers', players);
 
-  // Avisa os outros
+  // Avisa os outros que alguém entrou
   socket.broadcast.emit('newPlayer', { 
     id: socket.id, 
     player: players[socket.id] 
   });
 
-  // Movimento
+  // Movimento (Agora atualiza o thrust)
   socket.on('playerMovement', (movementData) => {
     if (players[socket.id]) {
       players[socket.id].x = movementData.x;
@@ -42,6 +43,7 @@ io.on('connection', (socket) => {
       players[socket.id].qy = movementData.qy;
       players[socket.id].qz = movementData.qz;
       players[socket.id].qw = movementData.qw;
+      players[socket.id].thrust = movementData.thrust; // Atualiza se está acelerando
       
       socket.broadcast.emit('playerMoved', {
         id: socket.id,
@@ -50,12 +52,18 @@ io.on('connection', (socket) => {
     }
   });
 
+  // --- NOVO: TIRO ---
+  socket.on('shoot', () => {
+    // Avisa todos (menos quem atirou) que esse jogador disparou
+    socket.broadcast.emit('playerShoot', socket.id);
+  });
+
   // --- COMBATE (PVP) ---
   socket.on('playerHit', (targetId) => {
     // Avisa a vítima que ela morreu
     io.to(targetId).emit('youDied');
-    // Avisa todo mundo (para tocar som de explosão se quiser)
-    io.emit('playerExploded', targetId);
+    // Avisa todo mundo para explodir a nave visualmente
+    io.emit('playerKilled', targetId);
   });
 
   socket.on('disconnect', () => {
